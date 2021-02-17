e35f434199d71932845ae46ef8045f8ee02f19c9
Clinton Garwood <clintongarwood@gmail.com>
2021-02-16 19:38:43 -0500

    react groceries

--- Homework/Mod2/week1/react-groceries/src/App.js
+++ Homework/Mod2/week1/react-groceries/src/App.js
@@ -12,24 +12,18 @@ const App = () => {
 		]
     );
 
-	const nameEl = React.useRef(null)
-	const brandEl = React.useRef(null)
-	const unitsEl = React.useRef(null)
-	const quantityEl = React.useRef(null)
-
-	const [inputValue, setInputValue] = useState(
-			{ itemName: '', brand: '', units: '', quantity: '', isPurchased: true }
-    );
+	const [inputValue, setInputValue] = useState({ isPurchased: true });
 
 	const handleChange = (event) => {
+		const value = event.target.value;
+
 		setInputValue({
-			itemName: nameEl.value,
-			brand: brandEl.value,
-			units: unitsEl.value,
-			quantity: quantityEl.value,
-			isPurchased: true
+			...inputValue,
+			[event.target.name]: value,
 		})
-		console.log(event.target.id, event.target.value)
+
+		// console.log(event.target.id, event.target.value)
+		// console.log(inputValue);
 	}
 
 	const handleSubmit = (event) => {
@@ -39,61 +33,70 @@ const App = () => {
 		const newItems = [newItem, ...items];
 
 		setItems(newItems);
-		setInputValue('');
+		setInputValue({ itemName: '', brand: '', units: '', quantity: '', isPurchased: true });
 
-		console.log(items);
+		console.log(newItems);
+	}
+
+	const handleDelete = (event) => {
+		
+		setItems(!items.isPurchased);
+
 	}
 
 	return (
 		<div className="container">
 			<div className="header">
 
+				{/* Header */}
 				<div className="header-title">
 					<h1>üShopper</h1>
 					<h4>The Grocery List You Won't Forget</h4>
 				</div>
 
+				{/* Submission Form */}
 				<div className="form-container">
-					<form onSubmit={handleSubmit}>
+					<form onSubmit={(event)=> handleSubmit(event)} onChange={(event)=> handleChange(event)}>
 						<h3>Remeber to pick up</h3>
 						<label htmlFor="item" value={items.name}></label>
-						<input ref={nameEl} value={inputValue.itemName} onChange={handleChange} className="addItemInput" id="item" placeholder="Item" style={{textAlign:"center"}}></input>
+						<input type="text" value={inputValue.itemName} name="itemName" id="item" placeholder="Item" style={{textAlign:"center"}}></input>
 						
 						<label htmlFor="brand" value={items.brand}></label>
-						<input ref={brandEl} value={inputValue.brand} onChange={handleChange} className="addItemInput" id="brand" placeholder="Brand" style={{textAlign:"center"}}></input>
+						<input type="text" value={inputValue.brand} name="brand" id="brand" placeholder="Brand" style={{textAlign:"center"}}></input>
 						
 						<label htmlFor="units" value={items.units}></label>
-						<input ref={unitsEl} value={inputValue.units} onChange={handleChange} className="addItemInput" id="units" placeholder="Units" style={{textAlign:"center"}}></input>
+						<input type="number" value={inputValue.units} name="units" id="units" placeholder="Units" style={{textAlign:"center"}}></input>
 						
 						<label htmlFor="quantity" value={items.quantity}></label>
-						<input ref={quantityEl} value={inputValue.quantity} onChange={handleChange} className="addItemInput" id="quantity" placeholder="Quantity" style={{textAlign:"center"}}></input>
+						<input type="number" value={inputValue.quantity} name="quantity" id="quantity" placeholder="Quantity" style={{textAlign:"center"}}></input>
 						
 						<label htmlFor="addItem" value={items.isPurchased}></label>
 						<input type="submit" value="Add to cart"></input>
 					</form>
 				</div>
 
+				{/* Grocery List */}
 				<div className="groceryList">
 					<h3 className="listName">Grocery List</h3>
 					{items.map((item, index) => item.isPurchased ? (
 						<form>
 								<label htmlFor="item" value={items.name}></label>
-								<input id="item" placeholder="Item" style={{textAlign:"center"}} value={items[index].itemName}></input>
+								<input id="item" placeholder="Item" style={{textAlign:"center"}} name="itemName" value={items[index].itemName}></input>
 
 								<label htmlFor="brand" value={items.brand}></label>
-								<input id="brand" placeholder="Brand" style={{textAlign:"center"}} value={items[index].brand}></input>
+								<input id="brand" placeholder="Brand" style={{textAlign:"center"}} name="brand" value={items[index].brand}></input>
 								
 								<label htmlFor="units" value={items.units}></label>
-								<input id="units" placeholder="Units" style={{textAlign:"center"}}value={items[index].units}></input>
+								<input id="units" placeholder="Units" style={{textAlign:"center"}}name="units" value={items[index].units}></input>
 								
 								<label htmlFor="quantity" value={items.quantity}></label>
-								<input id="quantity" placeholder="Quantity" style={{textAlign:"center"}} value={items[index].quantity}></input>
+								<input id="quantity" placeholder="Quantity" style={{textAlign:"center"}} name="quantity" value={items[index].quantity}></input>
 								
 								<label htmlFor="removeItem" value={items.isPurchased}></label>
-								<button>remove</button>  
+								<button id="removeItem" onClick={handleDelete}>remove</button>  
 						</form>
 					) : (
-						null
+						''
 					))}
 				</div>
 
